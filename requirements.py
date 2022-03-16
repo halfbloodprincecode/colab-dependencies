d7cade73a398a9b6d2f014e756313dbf58e1fe1a
server06 <101740277+server06@users.noreply.github.com>
2022-03-16 21:59:13 +0330

    0

--- requirements.py
+++ requirements.py
@@ -1,11 +1,9 @@
 import os
 from google.colab import drive
 
-def Drive(path=None):
+def Drive():
   if os.path.isdir('/content/drive'):
     drive.flush_and_unmount()
     drive.mount('/content/drive')
   else:
     drive.mount('/content/drive')
-  if path:
-    %cd {path}
