a977be4e74fcca2dfb6dcbd37347368eaeee54ec
server06 <101740277+server06@users.noreply.github.com>
2022-03-16 21:39:25 +0330

    util

--- util.py
+++ util.py
@@ -5,7 +5,5 @@ def Drive():
   if os.path.isdir('/content/drive'):
     drive.flush_and_unmount()
     drive.mount('/content/drive')
-    return 0
   else:
     drive.mount('/content/drive')
-    return 1
