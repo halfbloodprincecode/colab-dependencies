d5ccd73c2ec873517af376ecc3ee52bc40820eec
server06 <101740277+server06@users.noreply.github.com>
2022-03-16 21:32:20 +0330

    util

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
+import os
+from google.colab import drive
+
 def Drive():
-  from google.colab import drive
-  drive.mount('/content/drive')
-  drive.flush_and_unmount()
-  drive.mount('/content/drive')
+  if os.path.isdir('/content/drive'):
+    drive.flush_and_unmount()
+    drive.mount('/content/drive')
+  else:
+    drive.mount('/content/drive')
