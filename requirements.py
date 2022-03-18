7e0a3e824396e1a0d26ef130493aa84b87527bf3
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-18 19:37:27 +0330

    0

--- requirements.py
+++ requirements.py
@@ -12,8 +12,8 @@ def Drive():
 def Sys(paths=[]):
   [sys.path.insert(0, path) for path in paths]
 
-def Callback(params):
-  return {
+def Callback(params, fit=[], evaluate=[], predict=[], indexing=None):
+  cb = {
     'fit': [
       keras.callbacks.experimental.BackupAndRestore(backup_dir=f'{params["DIR_ROOT"]}/backup/params["model"]'),
       keras.callbacks.ModelCheckpoint(filepath='{params["DIR_ROOT"]}/experimental/params["model"]/weights/{int(epoch):09d}, {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
@@ -23,3 +23,8 @@ def Callback(params):
     'evaluate': [],
     'predict': []
   }
+  if indexing == None:
+    cb.fit.extend(fit)
+    cb.evaluate.extend(evaluate)
+    cb.predict.extend(predict)
+  return cb
