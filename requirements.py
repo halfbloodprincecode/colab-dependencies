54206a57d5cdcd9da1d2d0b7ddf83f057d130964
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-18 20:03:48 +0330

    0

--- requirements.py
+++ requirements.py
@@ -13,20 +13,3 @@ def Drive():
 
 def Sys(paths=[]):
   [sys.path.insert(0, path) for path in paths]
-
-def Callback(params, fit=[], evaluate=[], predict=[], indexing=None):
-  cb = {
-    'fit': [
-      keras.callbacks.experimental.BackupAndRestore(backup_dir=f'{params["DIR_ROOT"]}/backup/params["model"]'),
-      keras.callbacks.ModelCheckpoint(filepath='{params["DIR_ROOT"]}/experimental/params["model"]/weights/{int(epoch):09d}, {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
-      keras.callbacks.CSVLogger(filename=f'{params["DIR_ROOT"]}/experimental/params["model"]/metrics/fit.csv', separator=',', append=True),
-      keras.callbacks.ProgbarLogger(count_mode='steps', stateful_metrics=None),
-    ],
-    'evaluate': [],
-    'predict': []
-  }
-  if indexing == None:
-    cb['fit'].extend(fit)
-    cb['evaluate'].extend(evaluate)
-    cb['predict'].extend(predict)
-  return cb
