09efe372057f29ad11912e962402422384d24605
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-23 01:05:50 +0430

    0

--- tfModel.py
+++ tfModel.py
@@ -8,9 +8,9 @@ class Model(keras.Model):
   def getCallbacks(self, params, fit=[], evaluate=[], predict=[], indexing=None):
     cb = {
       'fit': [
-        keras.callbacks.BackupAndRestore(backup_dir=f'{params["DIR_ROOT"]}/backup/params["model"]'),
-        keras.callbacks.ModelCheckpoint(filepath='{params["DIR_ROOT"]}/experimental/params["model"]/weights/{int(epoch):09d}, {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
-        keras.callbacks.CSVLogger(filename=f'{params["DIR_ROOT"]}/experimental/params["model"]/metrics/fit.csv', separator=',', append=True),
+        keras.callbacks.BackupAndRestore(backup_dir=f'{params["DIR_ROOT"]}/backup/{params["model"]}'),
+        keras.callbacks.ModelCheckpoint(filepath=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/weights/{int(epoch):09d}, {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
+        keras.callbacks.CSVLogger(filename=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/metrics/fit.csv', separator=',', append=True),
         keras.callbacks.ProgbarLogger(count_mode='steps', stateful_metrics=None),
       ],
       'evaluate': [],
