d3c1b99eea81f363d9cd168a0ddb2e4511806f0d
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-22 15:25:24 +0430

    0

--- tfDataset.py
+++ tfDataset.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
 class Dataset:
-  def __new__(self, preprocessing, dpaths=[], classTags={}, B=32):
-    self.preprocessing = preprocessing
+  def __new__(self, preprocessing, classTags, B=32):
     self.classTags = classTags
     self.B = B
     self.D = []
+    dpaths = preprocessing.keys()
     for dpath in dpaths:
       d = tf.data.Dataset.list_files(dpath, shuffle=False)
-      d = d.shuffle(1000).map(self.preprocessing).prefetch(tf.data.AUTOTUNE).cache().batch(self.B)
+      d = d.shuffle(1000).map(preprocessing[dpath]).prefetch(tf.data.AUTOTUNE).cache().batch(self.B)
       self.D.append(d)
     return self.D
