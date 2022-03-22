b9019da04cd6c3c08d42ca88bf515480ab8317db
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-22 16:52:08 +0430

    0

---
+++ Normalization.py
@@ -0,0 +1,10 @@
+class Normalization:
+  @staticmethod
+  def normalize_img(img):
+    img = tf.cast(img, dtype=tf.float32)
+    # Map values in the range [-1, 1]
+    return (img / 127.5) - 1.0
+  
+  @staticmethod
+  def denormalize_img(img):
+    return (((img * 127.5) + 127.5).numpy()).astype(np.uint8)
