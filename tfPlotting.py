a4f002236c28624b14715cafa23123809244d38a
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-22 19:39:49 +0430

    0

--- tfPlotting.py
+++ tfPlotting.py
@@ -7,6 +7,9 @@ class Plotting:
   def subplots(zip, row=4, col=2, figsize=(10, 15)):
     _, ax = plt.subplots(row, col, figsize=figsize)
     for i, samples in enumerate(zip):
-      for j in range(col):
-        ax[i, j].imshow(Normalization.denormalize_img(samples[j][i]))
+      if col > 1:
+        for j in range(col):
+          ax[i, j].imshow(Normalization.denormalize_img(samples[j]))
+      else:
+        ax[i].imshow(Normalization.denormalize_img(samples[0]))
     plt.show()
