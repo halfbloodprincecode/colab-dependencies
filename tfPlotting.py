956a4ba547efaab3f626ac915f3702d449a91978
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-24 16:26:28 +0430

    0

--- tfPlotting.py
+++ tfPlotting.py
@@ -4,7 +4,7 @@ from tfNormalization import Normalization
 
 class Plotting:
   @staticmethod
-  def subplots(zip, row, col, figsize=(10, 15)):
+  def subplots(zip, row, col, R=[], figsize=(10, 15)):
     _, ax = plt.subplots(row, col, figsize=figsize)
     for i, samples in enumerate(zip):
       if row>1 and col>1:
@@ -12,19 +12,19 @@ class Plotting:
           img = samples[j]
           if img.shape[-1] == 1:
             img = img[:, :, 0]
-          ax[i, j].imshow(Normalization.denormalize_img(img))
+          ax[i, j].imshow(Normalization.denormalize_img(img, R=R[j]))
           ax[i, j].axis('off')
       elif row>1 and col<=1:
         img = samples[0]
         if img.shape[-1] == 1:
           img = img[:, :, 0]
-        ax[i].imshow(Normalization.denormalize_img(img))
+        ax[i].imshow(Normalization.denormalize_img(img, R=R[0]))
         ax[i].axis('off')
       elif row<=1 and col>1:
         for j in range(col):
           img = samples[j]
           if img.shape[-1] == 1:
             img = img[:, :, 0]
-          ax[j].imshow(Normalization.denormalize_img(img))
+          ax[j].imshow(Normalization.denormalize_img(img, R=R[j]))
           ax[j].axis('off')
     plt.show()
