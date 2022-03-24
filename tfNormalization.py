0bb334f3f1970742441d3376f02a4598c8f25226
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-24 17:24:32 +0430

    0

--- tfNormalization.py
+++ tfNormalization.py
@@ -12,8 +12,8 @@ class Normalization:
       return (img/255.)
   
   @staticmethod
-  def denormalize_img(img, R='-1to1'):
+  def denormalize_img(img, R='0to1'): # R: Range
     if R == '-1to1':
       return (((img * 127.5) + 127.5).numpy()).astype(np.uint8)
     elif R == '0to1':
-      return (img*255.)
+      return ((img*255.).numpy()).astype(np.uint8)
