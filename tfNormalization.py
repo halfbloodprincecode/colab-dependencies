0c22172acf47ff3c87f20307dd789ed34161ad73
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-24 16:21:52 +0430

    0

--- tfNormalization.py
+++ tfNormalization.py
@@ -3,11 +3,17 @@ import tensorflow as tf
 
 class Normalization:
   @staticmethod
-  def normalize_img(img):
+  def normalize_img(img, R='-1to1'):
     img = tf.cast(img, dtype=tf.float32)
-    # Map values in the range [-1, 1]
-    return (img / 127.5) - 1.0
+    if R == '-1to1':
+      # Map values in the range [-1, 1]
+      return (img / 127.5) - 1.0
+    elif R == '0to1':
+      return (img/255.)
   
   @staticmethod
-  def denormalize_img(img):
-    return (((img * 127.5) + 127.5).numpy()).astype(np.uint8)
+  def denormalize_img(img, R='-1to1'):
+    if R == '-1to1':
+      return (((img * 127.5) + 127.5).numpy()).astype(np.uint8)
+    elif R == '0to1':
+      return (img*255.)
