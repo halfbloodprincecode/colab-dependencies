aac18a69c149ccf0b0e8b492d23b041f22077d2f
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-28 13:23:37 +0430

    0

--- tfModel.py
+++ tfModel.py
@@ -10,7 +10,7 @@ class Model(keras.Model):
       'fit': [
         keras.callbacks.BackupAndRestore(backup_dir=f'{params["DIR_ROOT"]}/backup/{params["model"]}'),
         keras.callbacks.ModelCheckpoint(filepath=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/weights/'+'epoch: {epoch:02d}, val_loss: {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
-        keras.callbacks.CSVLogger(filename=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/metrics(fit)_epoch:'+'{epoch}.csv', separator=',', append=False),
+        keras.callbacks.CSVLogger(filename=(f'{params["DIR_ROOT"]}/experimental/{params["model"]}/metrics(fit)_epoch:'+'{epoch}.csv'), separator=',', append=False),
         keras.callbacks.ProgbarLogger(count_mode='steps', stateful_metrics=None),
       ],
       'evaluate': [],
