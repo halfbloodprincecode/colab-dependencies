f0ff2718ff9028c579b29f6db914bb759aade8e3
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-03-29 21:41:06 +0430

    0

--- tfModel.py
+++ tfModel.py
@@ -1,6 +1,7 @@
 import datetime
 import tensorflow as tf  
 from tensorflow import keras
+from tfCallback import Logger
 
 class Model(keras.Model):
   def __init__(self):
@@ -10,8 +11,9 @@ class Model(keras.Model):
     cb = {
       'fit': [
         keras.callbacks.BackupAndRestore(backup_dir=f'/content/backup/{params["model"]}'),
-        keras.callbacks.ModelCheckpoint(filepath=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/weights/'+'epoch: {epoch:02d}, val_loss: {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
-        keras.callbacks.CSVLogger(filename=params["DIR_ROOT"]+'/experimental/'+params["model"]+'/metrics(fit)_'+str(datetime.datetime.now())+'_.csv', separator=',', append=False),
+        Logger(path=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/fit.csv', parameters=params),
+#         keras.callbacks.ModelCheckpoint(filepath=f'{params["DIR_ROOT"]}/experimental/{params["model"]}/weights/'+'epoch: {epoch:02d}, val_loss: {val_loss:.2f}/weights', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', save_freq='epoch', options=None, initial_value_threshold=None),
+#         keras.callbacks.CSVLogger(filename=params["DIR_ROOT"]+'/experimental/'+params["model"]+'/metrics(fit)_'+str(datetime.datetime.now())+'_.csv', separator=',', append=False),
         keras.callbacks.ProgbarLogger(count_mode='steps', stateful_metrics=None),
       ],
       'evaluate': [],
