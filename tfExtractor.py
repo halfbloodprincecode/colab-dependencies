a9cf7624d42aea71dd4b06525da4e2c5b2466cd0
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-04-05 13:52:44 +0430

    0

--- tfExtractor.py
+++ tfExtractor.py
@@ -8,4 +8,4 @@ def fromModel(date, model, _inputs, layer_names):
   _outputs = [model.layers[i] for i in index]
   _outputs.append(model.layers[-1])
   extractor = keras.Model(inputs=_inputs, outputs=_outputs)
-  return extractor(date)
+  return extractor(date, training=False)
