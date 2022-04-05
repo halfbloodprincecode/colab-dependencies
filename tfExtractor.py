14e7c372c05dce978e8bdae53e85636a417a16b5
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-04-05 13:44:20 +0430

    0

---
+++ tfExtractor.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+from vis.utils import utils
+
+
+def fromModel(date, model, _inputs, layer_names):
+  index = [utils.find_layer_idx(model, name) for name in layer_names]
+  _outputs = [model.layers[i] for i in index]
+  _outputs.append(model.layers[-1])
+  extractor = keras.Model(inputs=_inputs, outputs=_outputs)
+  return extractor(date)
