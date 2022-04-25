c27d677330c0217d378620ca8fad9963832f85f9
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2022-04-26 01:06:57 +0430

    0

--- projection.py
+++ projection.py
@@ -1,4 +1,4 @@
 from sklearn.manifold import TSNE
 
 def tsne(X, n_components=2):
-  return TSNE(n_components=n_components, verbose=0, random_state=123).fit_transform(X)
+  return TSNE(n_components=n_components, verbose=0, random_state=123).fit_transform(X);
